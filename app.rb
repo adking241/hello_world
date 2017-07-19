3a26ec39ba5b11f57686cda1ae353ac9c03b73f0
ANITA KING <ADKING>
2017-07-19 00:19:48 -0400

    app file with name, age, nmbrs

--- app.rb
+++ app.rb
@@ -1,5 +1,119 @@
 require "sinatra"
 
-get '/' do 
+get '/'  do
+	redirect '/user_name'
+end
+
+get '/user_name' do
 	erb :index
 end
+
+post '/user_name' do
+	name_provided = params[:name_provided]
+    redirect '/age?name_provided=' + name_provided
+end
+
+get '/age' do
+	name_provided = params[:name_provided]
+	# puts "name_provided in get age #{name_provided}"
+	erb :age, :locals => {:name_provided => name_provided}
+end
+
+post '/age' do
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+	# puts "name_provided in get age #{name_provided} and age_provided is #{age_provided}"
+	redirect '/fave_nums?age_provided=' + age_provided + '&name_provided=' + name_provided
+
+end
+
+get '/fave_nums' do
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+	puts "age_provided in get fav nums #{age_provided}"
+	puts "name_provided in get fav nums #{name_provided}"
+	erb :fave_nums, :locals => {:age_provided => age_provided, :name_provided => name_provided}
+end
+
+post '/fave_nums' do
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+  	nums_provided = params[:input_nums]
+ #  	puts "age_provided in get nums #{age_provided}"
+	# puts "name_provided in get age #{name_provided}"
+	# puts "nums_provided in get age #{nums_provided}"
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require "sinatra"
+
+# get '/' do 
+# 	redirect '/user_name'
+# end
+
+# get '/user_name' do
+# 	erb :index
+# end
+
+# post '/user_name' do
+#  	name_provided = params[:input_age]
+#   	redirect '/age?name_provided=' + name_provided
+# end
+
+# get '/age' do
+# 	name_provided = params[:input_name]
+# 	puts "name_provided in get age #{name_provided}"
+# 	erb :age, :locals => {:name_provided =>name_provided}
+# end
+
+# post '/age' do 
+# 	age_provided = params[:input_age]
+# 	redirect '/fave_nums?user_age' + user_age
+# end
